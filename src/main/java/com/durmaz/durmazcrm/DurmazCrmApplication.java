5ade7b3298aa7dc7582f1c812c35ff1f2d28d2eb
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-06-21 11:01:35 +0300

    start project

---
+++ src/main/java/com/durmaz/durmazcrm/DurmazCrmApplication.java
@@ -0,0 +1,13 @@
+package com.durmaz.durmazcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DurmazCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DurmazCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/durmaz/durmazcrm/DurmazCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durmaz.durmazcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurmazCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

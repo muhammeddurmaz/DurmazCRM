986a20d5f816dce3db17b8bfab4b354d3842da53
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-06-22 21:25:03 +0300

    added BaseEntity

---
+++ src/main/java/com/durmaz/durmazcrm/model/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.durmaz.durmazcrm.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name = "created_user")
+    protected String createdUser;
+
+    @Column(name = "created_at")
+    protected LocalDateTime createdAt;
+
+    @Column(name = "updated_user")
+    protected String updatedUser;
+
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
